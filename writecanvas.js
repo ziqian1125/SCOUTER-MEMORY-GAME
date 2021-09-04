4691e58272dd6109da545d9324a718ea5b0fd826
ziqian1125 <71480117+ziqian1125@users.noreply.github.com>
2021-09-04 19:01:55 +0800

    import writecanvas js in write html files

---
+++ writecanvas.js
@@ -0,0 +1,27 @@
+var cv = document.getElementById("cv");
+var ctx = cv.getContext('2d');
+cv.onmousedown = function (ev) {
+    var ev = ev || window.event;
+    ctx.moveTo(ev.clientX - cv.offsetLeft, ev.clientY - cv.offsetTop);
+    document.onmousemove = function (ev) {
+        var ev = ev || window.event;
+        ctx.lineTo(ev.clientX - cv.offsetLeft, ev.clientY - cv.offsetTop);
+        ctx.stroke();
+    }
+    document.onmouseup = function () {
+        document.onmousemove = null;
+        document.onmouseup = null;
+    }
+
+    // var dataImg = new Image()
+    // dataImg.src = cv.toDataURL('image/png')
+    // document.body.appendChild(dataImg) 
+}
+
+function copyText(cv){
+    var dataImg = new Image()
+    dataImg.src = cv.toDataURL('image/png')
+    document.body.appendChild(dataImg) 
+}
+
+cv.crossOrigin = 'anonymous'
